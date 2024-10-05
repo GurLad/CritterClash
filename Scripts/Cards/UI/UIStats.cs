8e2ebbc5821d701f59847f096deff558033d3391
Gur Ladizhinsky <40135047+GurLad@users.noreply.github.com>
2024-10-06 01:53:04 +0300

    Finished UI stats

--- Scripts/Cards/UI/UIStats.cs
+++ Scripts/Cards/UI/UIStats.cs
@@ -14,12 +14,9 @@ public partial class UIStats : Node
     {
         HealthRect.Visible = !string.IsNullOrEmpty(stats.Health);
         Health.Text = stats.Health;
-        Health.Position += Vector2.Right * (stats.Health.Length > 1 ? 1 : 0);
         AttackRect.Visible = !string.IsNullOrEmpty(stats.Health);
         Attack.Text = stats.Health;
-        Attack.Position += Vector2.Right * (stats.Attack.Length > 1 ? 1 : 0);
         SpeedRect.Visible = !string.IsNullOrEmpty(stats.Health);
         Speed.Text = stats.Health;
-        Speed.Position += Vector2.Right * (stats.Speed.Length > 1 ? 1 : 0);
     }
 }
