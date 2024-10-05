f609057ef85b263ad5a565d03be7782c3cd272d0
Gur Ladizhinsky <40135047+GurLad@users.noreply.github.com>
2024-10-06 01:20:58 +0300

    Added main & stuff

--- Scripts/Cards/UI/UICard.cs
+++ Scripts/Cards/UI/UICard.cs
@@ -3,6 +3,9 @@ using System;
 
 public partial class UICard : Control
 {
+    [Export] private Label NameField;
+    [Export] private TextureRect IconRect;
+
     public ACard Card { get; private set; }
     public bool Enemy { get; private set; } = false; // For now, single-player
     public int Index { get; set; }
