12ad9b83597d08f831982bd60546786bcda0b25e
Gur Ladizhinsky <40135047+GurLad@users.noreply.github.com>
2024-10-05 18:50:26 +0300

    Finished the body loader

--- Scripts/Bodies/Loading/BodyLoader.cs
+++ Scripts/Bodies/Loading/BodyLoader.cs
@@ -3,4 +3,31 @@ using System;
 
 public partial class BodyLoader : Node
 {
+    public override void _Ready()
+    {
+        base._Ready();
+        foreach (var child in GetChildren())
+        {
+            if (child is BodyLoaderNode node)
+            {
+                BodyRecord record = BodyRecords.Find(a => a.Name == child.Name);
+                if (record != null)
+                {
+                    record.AttachLoaderNode(node);
+                }
+                else
+                {
+                    GD.PrintErr("[BodyPartLoader]: Loader node without a record!");
+                }
+            }
+            else
+            {
+                GD.PrintErr("[BodyPartLoader]: Invalid loader node!");
+            }
+        }
+        if (BodyRecords.FindIndex(a => !a.Inited) >= 0)
+        {
+            GD.PrintErr("[BodyPartLoader]: Record without a loader node!");
+        }
+    }
 }
