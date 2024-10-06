7c22f646afbbe301e38468b654007e131244ae75
Gur Ladizhinsky <40135047+GurLad@users.noreply.github.com>
2024-10-06 19:08:58 +0300

    Working on player ui animations

--- Scripts/Players/PlayerHuman.cs
+++ Scripts/Players/PlayerHuman.cs
@@ -6,17 +6,51 @@ public partial class PlayerHuman : APlayerController
     [Export] private UICardPlacementGrid PlacementGrid;
     [Export] private UIHand Hand;
     [Export] private Control PlayerUI;
+    [Export] private Control MouseBlocker;
+    [ExportCategory("Animation")]
+    [Export] private float AnimationTime = 1;
+    [Export] private Vector2 AnimationPosOffset = Vector2.Down * 150;
+
+    private Interpolator Interpolator = new Interpolator();
+    private Vector2 BasePosition;
+
+    public override void _Ready()
+    {
+        AddChild(Interpolator);
+        BasePosition = PlayerUI.Position;
+        PlayerUI.Position = BasePosition + AnimationPosOffset;
+        PlayerUI.Visible = false;
+        base._Ready();
+    }
 
     public void FinishTurn()
     {
-        PlayerUI.Visible = false;
-        EmitFinishTurn();
+        MouseBlocker.Visible = true;
+        Interpolator.Interpolate(AnimationTime,
+            new Interpolator.InterpolateObject(
+                a => PlayerUI.Position = a,
+                PlayerUI.Position,
+                BasePosition + AnimationPosOffset,
+                Easing.EaseInSin));
+        Interpolator.OnFinish = () =>
+        {
+            MouseBlocker.Visible = false;
+            PlayerUI.Visible = false;
+            EmitFinishTurn();
+        };
     }
 
     protected override void BeginTurnInternal()
     {
-        // Animate....
+        MouseBlocker.Visible = true;
         PlayerUI.Visible = true;
+        Interpolator.Interpolate(AnimationTime,
+            new Interpolator.InterpolateObject(
+                a => PlayerUI.Position = a,
+                PlayerUI.Position,
+                BasePosition,
+                Easing.EaseInSin));
+        Interpolator.OnFinish = () => MouseBlocker.Visible = false;
     }
 
     protected override void ConnectDeckInternal(Deck deck)
