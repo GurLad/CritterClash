d21788dbc66a18377fc6e553f1d0cb01785c63e9
Gur Ladizhinsky <40135047+GurLad@users.noreply.github.com>
2024-10-06 17:40:06 +0300

    Working on card UI

--- Scripts/Cards/UI/UICardRenderer.cs
+++ Scripts/Cards/UI/UICardRenderer.cs
@@ -11,14 +11,18 @@ public partial class UICardRenderer : Control
     [Export] private TextureRect IconRect;
     [Export] private UIStats UIStats;
     [Export] private Container UIStatsContainer;
+    [ExportCategory("Colours")]
+    [Export] private Color FlavourColour;
+    [Export] private Color DescriptionColour;
 
     public void Render(ACard card)
     {
         CostLabel.Text = card.Cost.ToString();
         NameLabel.Text = card.Name;
-        DescriptionLabel.Text = card.Description;
-        DescriptionContainer.Visible = !string.IsNullOrEmpty(card.Description);
-        FlavourTextLabel.Text = card.FlavourText;
+        FlavourTextLabel.Text = !string.IsNullOrEmpty(card.Description) ? card.Description : card.FlavourText;
+        FlavourTextLabel.SelfModulate = (!string.IsNullOrEmpty(card.Description) ? DescriptionColour : FlavourColour);
+        //DescriptionContainer.Visible = !string.IsNullOrEmpty(card.Description);
+        //FlavourTextLabel.Text = card.FlavourText;
         IconRect.Texture = card.CardIcon;
         UIStatsContainer.Visible = UIStats.Render(card.Stats);
     }
